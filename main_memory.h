06e9e0015b2e88daac1c50f09f6906cd99422c34
amolpai <35805901+amolpai@users.noreply.github.com>
2020-03-17 00:05:45 +0530

    Add files via upload

--- main_memory.h
+++ main_memory.h
@@ -8,3 +8,4 @@ extern page_table_struct* mm_initialize_page_table(int, main_memory_struct**);
 extern main_memory_struct* mm_initialize_mm(void);
 extern void mm_load_page(int, logical_address, page_table_struct**, main_memory_struct**);
 extern logical_address mm_convert(int);
+extern int mm_search_page_table(logical_address, int, page_table_struct**, main_memory_struct**);
