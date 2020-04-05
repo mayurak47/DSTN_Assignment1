35c50fd3ff0bfa07c755b1bd19df6ccdbfb9724d
2017A7PS0038G <f20170038@goa.bits-pilani.ac.in>
2020-04-05 15:53:46 +0530

    Small changes

--- Group 1/cpu.c
+++ Group 1/cpu.c
@@ -233,18 +233,16 @@ int main(int argc, char* argv[]){
     fclose(output_rates_file);
     fclose(output_times_file);
 
-    for(int i=0; i<NUM_PROCESSES; i++){
-        free(tlb_hit);
-        free(tlb_miss);
-        free(l1_hit);
-        free(l1_miss);
-        free(l2_hit);
-        free(l2_miss);
-        free(page_fault);
-        free(page_hit);
-        free(terminate_process);
-        free(instruction_count);
-    }
+    free(tlb_hit);
+    free(tlb_miss);
+    free(l1_hit);
+    free(l1_miss);
+    free(l2_hit);
+    free(l2_miss);
+    free(page_fault);
+    free(page_hit);
+    free(terminate_process);
+    free(instruction_count);
 
 }
 
